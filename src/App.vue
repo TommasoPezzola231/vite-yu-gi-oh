b8a17e449ce41b7b0c52fc3f6009519e19dd4cfd
TommasoPezzola <pezzola123@gmail.com>
2023-05-23 16:15:46 +0200

    Error

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
-<script setup>
+<script>
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
-
 import axios from 'axios'
+import { store } from './data/store.js'
+
+export default {
+  name: "App",
+  data() {
+    return {
+      store: store
+    }
+  },
+  mounted() {
+    axios.get(store.urlAPI).then(result => {
+      console.log(result.data.data)
+      this.store.cards.push(result.data.data)
+      console.log("secondo console log:" + this.store.cards)
+    })
+  }
+}
+
 
 </script>
 
