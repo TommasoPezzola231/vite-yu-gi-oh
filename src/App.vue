8cefc3665f01c4f36398d85ca617bb91c27e3791
TommasoPezzola <pezzola123@gmail.com>
2023-05-23 15:14:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+import axios from 'axios'
+
+</script>
+
+<template>
+  <AppHeader />
+
+  <AppMain />
+</template>
+
+<style lang="scss" scoped></style>
