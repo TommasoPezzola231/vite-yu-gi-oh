bf2dfcbd90ae4e27cd6538ceed46ae214ddbf5ae
TommasoPezzola <pezzola123@gmail.com>
2023-05-23 15:20:28 +0200

    feat,style: add header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,6 +4,16 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container">
+            <h1>Yu-Gi-Oh Api</h1>
+        </div>
+    </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+    padding: 0.5rem 0;
+}
+</style>
